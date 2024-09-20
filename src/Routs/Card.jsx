50a24aa8533c070fc36f65d84333af24ea4dbd59
arav39marri <aravindmarripelli@gmail.com>
2024-09-20 15:10:54 +0530

    changes

--- src/Routs/Card.jsx
+++ src/Routs/Card.jsx
@@ -4,7 +4,7 @@ const Card = (props) => {
   return (
     <div className="card h-fit flex w-80 p-10  flex-col border ml-10 justify-between border-gray-300 rounded-lg">
   <h2 className='text-red-500'>{props.name}</h2>
-  <h3>{props.title}</h3>
+  <h3>{props.caption}</h3>
   <p>{props.email}</p>
   <p>{props.about}</p>
   
